1625c2f9f5dc63f372299bf7f37edbd60946f19f
Fede046 <malserfede@gmail.com>
2025-03-09 21:47:50 +0100

    i

---
+++ BFS/src/main/java/org/example/BFS.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class BFS {
+    public static Tree search(int value,Tree root){
+        Queue<Tree> queue = new ArrayDeque<>();
+        queue.add(root); //first we add the root node as th
+        //only member of the queue
+
+
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Tree root = new Tree(10);
+        Tree newChild
+    }
+}
